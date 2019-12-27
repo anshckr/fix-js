f467a577f817ff44dabcf6062133aeed4a8b283d
Anshul Nema <anshul.n@browserstack.com>
2019-12-27 12:57:23 +0530

    add istanbul coverage reporter

--- test-setup.spec.js
+++
@@ -1,9 +0,0 @@
-// const sinon = require('sinon');
-
-// beforeEach(() => {
-//   this.sandbox = sinon.sandbox.create();
-// });
-
-// afterEach(() => {
-//   this.sandbox.restore();
-// });
--- test/transforms/unused-assigned-vars.spec.js
+++ test/transforms/unused-assigned-vars.spec.js
@@ -1,23 +1,32 @@
 const fs = require('fs');
 
-const sinon = require('sinon');
 const chai = require('chai');
+const sinon = require('sinon');
 
 const { expect } = chai;
 const sinonChai = require('sinon-chai');
 
 chai.use(sinonChai);
 
-const transformUnusedAssignedVars = require('./unused-assigned-vars');
+const transformUnusedAssignedVars = require('../../transforms/unused-assigned-vars');
 
 describe('unused-assigned-vars transformer', () => {
+  beforeEach(() => {
+    this.sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    this.sandbox.restore();
+  });
+
   it('remove unused initialized variables', () => {
     const filePath = 'sample.js'; // can be anything as we are stubbing fs.readFileSync
-    sinon.stub(fs, 'readFileSync').callsFake(() => {
+
+    this.sandbox.stub(fs, 'readFileSync').callsFake(() => {
       return 'function test() { i = 0; }';
     });
 
-    const result = transformUnusedAssignedVars(filePath, false);
+    const result = transformUnusedAssignedVars(filePath);
 
     expect(result).to.eql('function test() {}');
   });
